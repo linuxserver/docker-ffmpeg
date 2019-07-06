ceb78ac245009668b41abbb6fc4cbf3b3a6158e1
thelamer <ryankuba@gmail.com>
2019-07-06 05:25:57 +0000

    stashing work, added command example page and functional config loading and saving, skeleton for background job processing

--- public/ffmpeg-web.js
+++ public/ffmpeg-web.js
@@ -4,78 +4,240 @@ var port = window.location.port;
 var protocol = window.location.protocol;
 var socket = io.connect(protocol + '//' + host + ':' + port, {});
 
+
 // On connect render the main page
 $(document).ready(function(){rendermain()})
 
 //// Main Page rendering ////
 function rendermain(){
-  $('#pagecontent').empty();
+  pagepurge();
   $('#pagecontent').append('<center><div class="spinner-border" style="width: 6rem; height: 6rem;"></div></center>');
   socket.emit('getmain');
 }
-socket.on('sendmain', function(rules){
-  $('#pagecontent').empty();
+socket.on('sendmain', function(data){
+  pagepurge();
+  $('#pagecontent').append('<center>Processed jobs here and current status</center>');
+});
+
+//// Config Page rendering ////
+function renderconfig(){
+  pagepurge();
+  $('#pagecontent').append('<center><div class="spinner-border" style="width: 6rem; height: 6rem;"></div></center>');
+  socket.emit('getconfig');
+}
+socket.on('sendconfig', function(rules){
+  pagepurge();
+  var interval = rules.interval;
+  $('#headerform').append('\
+    <select class="custom-select form-control mr-sm-2" id="interval">\
+      <option selected value="' + interval + '">' + interval + '</option>\
+      <option value"Never">Never</option>\
+      <option value="30">30 seconds</option>\
+      <option value="60">60 seconds</option>\
+      <option value="300">5 minutes</option>\
+    </select>\
+    <button style="cursor:pointer;" onclick="saveall()" class="btn btn-primary my-2 my-sm-0" type="submit">Save Config</button>\
+  ');
   var editor = [];
   $(rules.commands).each(function(i,data){
     var name = data.name;
     var extension = data.extension;
     var command = data.command;
+    var cleanup = data.delete;
+    if (cleanup == true){
+      var checkbox = '<input class="form-check-input" type="checkbox" id="delete' + i + '" checked>'
+    }
+    else{
+      var checkbox = '<input class="form-check-input" type="checkbox" id="delete' + i + '">'
+    }
     $('#pagecontent').append('\
-      <div class="card" id="' + name + '">\
+      <div class="card rule" id="' + i + '">\
         <div class="card-body">\
-          <form>\
-            <div class="form-row align-items-center">\
-              <div class="col-auto">\
-                <input type="text" class="form-control" value="' + name + '" placeholder="File Extension">\
-              </div>\
-              <div class="col-auto">\
-                <input type="text" class="form-control" value="' + extension + '" placeholder="File Extension">\
-              </div>\
-              <div class="col-auto">\
-                <div class="form-check mb-2">\
-                  <input class="form-check-input" type="checkbox" id="deletesource">\
-                  <label class="form-check-label" for="deletesource">\
-                    Delete Source\
-                  </label>\
-                </div>\
-              </div>\
-              <div class="col float-right">\
-                <button class="btn btn-success mb-2 float-right">Run Single</button>\
+          <div class="form-row align-items-center">\
+            <div class="col-auto">\
+              <input type="text" class="form-control" value="' + name + '" placeholder="Rule Name" id="name' + i + '">\
+            </div>\
+            <div class="col-auto">\
+              <input type="text" class="form-control" value="' + extension + '" placeholder="File Extension" id="extension' + i + '">\
+            </div>\
+            <div class="col-auto">\
+              <div class="form-check mb-2">\
+                '+ checkbox +'\
+                <label class="form-check-label" for="delete' + i + '">\
+                  Delete Source\
+                </label>\
               </div>\
             </div>\
-          </form>\
+          </div>\
           <br>\
           <div id="editor' + i + '" style="height: 250px; width: 100%"></div>\
         </div>\
       </div>\
       <br>\
     ').promise().done(function(){
-        editor[i] = ace.edit("editor" + i);
-        editor[i].setTheme("ace/theme/chrome");
-        editor[i].session.setMode("ace/mode/sh");
+        editor[i] = ace.edit('editor' + i);
+        editor[i].setTheme('ace/theme/chrome');
+        editor[i].session.setMode('ace/mode/sh');
         editor[i].$blockScrolling = Infinity;
         editor[i].setOptions({
           readOnly: false,
         });
         editor[i].setValue(command, -1);
     });
-  }).promise().done(function(){
-      $('#pagecontent').append('<center><button type="button" class="btn btn-secondary btn-lg">+</button></center>');
   });
+  $('#pagefooter').append('<center><button type="button" class="btn btn-secondary btn-lg" onclick="addconfig()">+</button></center>');
 });
+// Add config rule slots to the page
+function addconfig(){
+  var rand = Math.random().toString(36).replace('0.', '');
+  $('#pagecontent').append('\
+    <div class="card rule" id="' + rand + '">\
+      <div class="card-body">\
+        <div class="form-row align-items-center">\
+          <div class="col-auto">\
+            <input type="text" class="form-control" placeholder="Rule Name" id="name' + rand + '">\
+          </div>\
+          <div class="col-auto">\
+            <input type="text" class="form-control" placeholder="File Extension" id="extension' + rand + '">\
+          </div>\
+          <div class="col-auto">\
+            <div class="form-check mb-2">\
+              <input class="form-check-input" type="checkbox" id="delete' + rand + '">\
+              <label class="form-check-label" for="delete' + rand + '">\
+                Delete Source\
+              </label>\
+            </div>\
+          </div>\
+        </div>\
+        <br>\
+        <div id="editor' + rand + '" style="height: 250px; width: 100%"></div>\
+      </div>\
+    </div>\
+    <br>\
+  ').promise().done(function(){
+      editor = ace.edit('editor' + rand);
+      editor.setTheme('ace/theme/chrome');
+      editor.session.setMode('ace/mode/sh');
+      editor.$blockScrolling = Infinity;
+      editor.setOptions({
+        readOnly: false,
+      });
+  });
+}
+// Save all config data
+function saveall(){
+  var interval = $('#interval').val();
+  var dataset = {'interval':interval,'commands':[]};
+  var ids = $('.rule').map(function(){
+      return this.id;
+  }).get();
+  $(ids).each(function(i,id){
+    var name = $('#name' + id).val();
+    var extension = $('#extension' + id).val();
+    var cleanup = $('#delete' + id).prop('checked');
+    var editor = ace.edit("editor" + id);
+    var command = editor.getValue();
+    var single_command = {'name':name,'extension':extension,'delete':cleanup,'command':command};
+    dataset.commands.push(single_command);
+  }).promise().done(function(){
+    socket.emit('saveconfig',dataset);
+  });
+}
+
+
+//// Terminal Page rendering ////
+function renderterminal(){
+  pagepurge();
+  $('#pagecontent').append('<center><div class="spinner-border" style="width: 6rem; height: 6rem;"></div></center>');
+  socket.emit('getterminal');
+}
+socket.on('sendterminal', function(data){
+  pagepurge();
+  $('#pagecontent').append('<center>Raw bash terminal here for users to test commands</center>');
+});
+
+///////////////////////// test function client side //////////////////////////////////////
+socket.on('testout', function(out){
+  console.log(out);
+});
+///////////////////////// REMOVE FROM RELEASE ////////////////////////////////////////////
 
 //// Command Page rendering ////
 function rendercommands(){
-  $('#pagecontent').empty();
+  pagepurge();
   $('#pagecontent').append('<center><div class="spinner-border" style="width: 6rem; height: 6rem;"></div></center>');
   socket.emit('getcommands');
 }
 socket.on('sendcommands', function(categories){
-  console.log(categories);
-  $('#pagecontent').empty();
+  pagepurge();
   var editor = [];
   $('#pagecontent').append('<div id="cats"></div>')
   $(categories).each(function(i,data){
+    var commands = data.commands;
     var cat = data.category;
+    var catid = cat.replace(/ |\./g,'');
+    $('#cats').append('\
+      <div class="card">\
+        <div class="card-header" id="' + catid + '" data-toggle="collapse" data-target="#' + catid + '_list" aria-expanded="true" aria-controls="' + catid + '_list">\
+          ' + cat + '\
+        </div>\
+      </div>');
+    $('#cats').append('\
+      <div id="' + catid + '_list" class="collapse" aria-labelledby="' + catid + '" data-parent="#cats">\
+        <div class="card-body btn-toolbar" id="' + catid + '_commands">\
+        </div>\
+      </div>').promise().done(
+        function(){
+        $(commands).each(function(i,data){
+          var name = data.name;
+          var nameid = name.replace(/ |\./g,'');
+          var description = btoa(data.description);
+          var command = btoa(data.command);
+          $('#' + catid + '_commands').append('\
+            <button type="button" \
+              id="' + nameid + '_button" \
+              data-toggle="modal" \
+              data-target="#modal" \
+              style="cursor:pointer;" \
+              onclick="commandmodal(\'' + nameid + '\')"\
+              class="btn btn-secondary btn-lg mx-auto" \
+              data-name="' + name + '"\
+              data-description="' + description + '"\
+              data-command="' + command + '"\
+              >' + name + '\
+            </button>');
+        });
+      });
   });
 });
+function commandmodal(nameid){
+  modalpurge();
+  var name = $('#' + nameid + '_button').data('name')
+  var description = atob($('#' + nameid + '_button').data('description'));
+  var command = atob($('#' + nameid + '_button').data('command'));
+  $('#modaltitle').append(name);
+  $('#modalbody').append('<p>' + description + '</p>');
+  $('#modalbody').append('<div id="editor" style="height: 250px; width: 100%"></div>'
+    ).promise().done(function(){
+        editor = ace.edit("editor");
+        editor.setTheme("ace/theme/chrome");
+        editor.session.setMode("ace/mode/sh");
+        editor.$blockScrolling = Infinity;
+        editor.setOptions({
+          readOnly: true,
+        });
+        editor.setValue(command, -1);
+    });
+}
+
+
+
+function modalpurge(){
+  $('#modaltitle').empty();
+  $('#modalbody').empty();
+}
+function pagepurge(){
+  $('#headerform').empty();
+  $('#pagecontent').empty();
+  $('#pagefooter').empty();
+}
--- server.py
+++ server.py
@@ -2,37 +2,102 @@ from aiohttp import web
 import socketio
 import yaml
 import os
+import glob
+import re
+import time
 
-sio = socketio.AsyncServer()
+# Websocket server
+sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
 
+
+
+################################
+#        Job functions         #
+################################
+
+# Build and return an array of stuff to process
+def build_list(extension):
+  # Sanitize exenstion
+  extension_regex = re.sub(r'([A-Za-z])', lambda m: '[' + m.group(1).upper() + m.group(1).lower() + ']', extension)
+  # Build full glob
+  all_files = glob.glob('/in/**/*' + extension_regex, recursive=True)
+  # Check if anything in this array has a processed log or is not a file and pull it out
+  for file in all_files:
+    if not os.path.isfile(file) or os.path.isfile(file + '.ffmpeg_log'):
+      all_files.remove(file)  
+  return all_files
+
+# Background job thread loop for file processing
+async def processor():
+  while True:
+    files = build_list('.mkv')
+    await sio.emit('testoutt', files)
+    await sio.sleep(5)
+sio.start_background_task(processor)
+
+################################
+#         Web Server           #
+################################
+
+# Default returns for static files and index root
 async def index(request):
   with open('./public/index.html') as f:
     return web.Response(text=f.read(), content_type='text/html')
-
 app.router.add_get('/', index)
 app.router.add_static('/public/', path=str('./public/'))
 
 # Send the current config to the user to render
-@sio.on('getmain')
-async def message(sid):
+@sio.on('getconfig')
+async def config(sid):
   with open("./config.yml", 'r') as stream:
     try:
       config = yaml.safe_load(stream)
-      await sio.emit('sendmain', config, room=sid)
+      await sio.emit('sendconfig', config, room=sid)
     except yaml.YAMLError as e:
       print(e)
 
 # Send the current command examples from github to the user to render
 @sio.on('getcommands')
-async def message(sid):
+async def commands(sid):
   with open("./commands.yml", 'r') as stream:
     try:
       commands = yaml.safe_load(stream)
       await sio.emit('sendcommands', commands, room=sid)
     except yaml.YAMLError as e:
       print(e)
+
+# Main page for rendering processing history and current
+@sio.on('getmain')
+async def commands(sid):
+  data = 'test'
+  await sio.emit('sendmain', data, room=sid)
+      
+# Save user set config
+@sio.on('saveconfig')
+async def commands(sid, data):
+  with open("/config/config.yml", 'w') as configfile:
+    try:
+      yaml.dump(data, configfile)
+    except yaml.YAMLError as e:
+      print(e)
+
+
+################################
+#         User Terminal        #
+################################
+
+# Terminal page rendering
+@sio.on('getterminal')
+async def commands(sid):
+  data = 'test'
+  await sio.emit('sendterminal', data, room=sid)
+
+
+################################
+#           App Run            #
+################################
 
 if __name__ == '__main__':
     web.run_app(app, port=8787)
